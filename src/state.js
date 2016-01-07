0b3cc46703718d84d496886ad8ce4c33bf74f5f4
Kenrick Beckett <kenrick.beckett@gmail.com>
2016-01-06 20:10:14 -0500

    Change game state to use Records instead of Maps

--- src/state.js
+++ src/state.js
@@ -1,30 +1,57 @@
-import { Map, List } from 'immutable';
+import { Map, List, Record } from 'immutable';
 import { TEAMS } from './constants'
 import { range, flatten } from 'lodash';
 
-function generatePlayers(playerCount) {
+const Player = Record({
+  id: undefined,
+  team: undefined
+});
+
+const Token = Record({
+  id: undefined,
+  team: undefined,
+  coord: List.of(0, 0),
+  active: false,
+  ascended: false
+});
+
+const Action = Record({
+  type: undefined,
+  playerId: undefined,
+});
+
+const State = Record({
+  players: List(),
+  tokens: List(),
+  actions: List(),
+  playerTurn: 0,
+  winner: undefined
+});
+
+function createPlayers(playerCount) {
   return range(playerCount).map((id) => (
-    Map({ id: id, team: TEAMS[id] })
+    createRecord(Player, { id: id, team: TEAMS[id] })
   ));
 }
 
-function generateTokens(playerCount) {
-  return flatten(range(playerCount).map((id) => {
-    const team = TEAMS[id];
-
-    return range(4).map(() => (
-      Map({ team: team, cord: List.of(0, 0), isActive: false, isAscended: false })
-    ));
-  })).map((token, id) => token.set('id', id));
+function createTokens(playerCount) {
+  return flatten(range(playerCount).map((id) => (
+    range(4).map(() => createRecord(Token, { team: TEAMS[id] }))
+  ))).map((token, id) => token.set('id', id));
 }
 
-export function initState(playerCount) {
-  return Map({
-    players: List(generatePlayers(playerCount)),
-    tokens: List(generateTokens(playerCount)),
-    actions: List(),
-    playerTurn: 0,
-    winner: undefined,
+function createRecord(record, spec = {}) {
+  return new record(spec);
+}
+
+export function createAction(spec) {
+  return createRecord(Action, spec);
+}
+
+export function createState(playerCount) {
+  return createRecord(State, {
+    players: List(createPlayers(playerCount)),
+    tokens: List(createTokens(playerCount)),
   });
 }
 
--- test/state_spec.js
+++ test/state_spec.js
@@ -1,24 +1,24 @@
 import expect from 'expect';
-import { initState } from '../src/state.js';
+import { createState } from '../src/state.js';
 
 describe("state module", () => {
-  const state = initState(2);
+  const state = createState(2);
 
-  describe("initState", () => {
+  describe("createState", () => {
     it('has same amount of players passed in', () => {
-      expect(state.get('players').count()).toEqual(2);
+      expect(state.players.count()).toEqual(2);
     });
 
     it('has 4 tokens for each player', () => {
-      expect(state.get('tokens').count()).toEqual(8);
+      expect(state.tokens.count()).toEqual(8);
     });
 
     it('the initial player turn is 0', () => {
-      expect(state.get('playerTurn')).toEqual(0);
+      expect(state.playerTurn).toEqual(0);
     });
 
     it('has winner set to undefined', () => {
-      expect(state.get('winner')).toBe(undefined);
+      expect(state.winner).toBe(undefined);
     });
   });
 });
