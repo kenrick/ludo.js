89c795938f13b9a5bdc5873b1542ea670a38ad7d
Kenrick Beckett <kenrick.beckett@gmail.com>
2016-01-06 20:15:18 -0500

    Add integration test for the createGame api

---
+++ test/ludo_spec.js
@@ -0,0 +1,20 @@
+import expect from 'expect';
+import { createGame } from '../index'
+
+describe('ludo game', () => {
+  it('returns a dice roll action for the first action type', (done) => {
+    let action = undefined;
+    createGame({
+      playerCount: 2,
+      input({ type, roll, finder }, i) {
+        expect(type).toEqual('dice roll');
+        action = roll([1, 2]);
+        i(action);
+      },
+      output(state, c) {
+        expect(state.get('actions').last().equals(action)).toBe(true);
+        done();
+      }
+    });
+  });
+});
